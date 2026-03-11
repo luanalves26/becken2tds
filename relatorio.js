f77607f06d676a0624f5e50d7cb5b807a85e979e
luanalves26 <luan.alves26@escola.pr.gov.br>
2026-03-11 08:52:07 -0300

    Add files via upload

---
+++ relatorio.js
@@ -0,0 +1,9 @@
+//Relatório e notas
+let aluno = "Gabriel";
+let nota1 = 8.5;
+let nota2 = 5.0;
+
+let media = (nota1 + nota2) / 2;
+let relatorio = "Aluno: " + aluno +" | Notas "  +  nota1 + " é " + nota2 + " | Media de notas" + media;
+console.log("relatorio de Notas");
+console.log(relatorio);
