cacbe5f982fe9677c4d610c04a43335431f94aea
luan <luan.alves26@escola.pr.gov.br>
2026-05-13 13:54:29 +0000

    versao1

---
+++ desafio.js
@@ -0,0 +1,18 @@
+//⚫Declarar 5 variaveis para representar um funcionario
+
+let nomeFuncionario = "felipe Silva";
+let cargo = "Desenvolvedor";
+let salario = 4500.00;
+let idade = 28;
+let estaAtivo = true;
+
+let numero1 = 5;
+let numero2 = 10;
+
+// Exibindo a soma
+let soma = numero1 + numero2;
+console.log(soma); // Resultado: 15
+
+// Exibindo a multiplicação
+let multiplicacao = numero1 * numero2;
+console.log(multiplicacao); // Resultado: 50
